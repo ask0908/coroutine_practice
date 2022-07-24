9c4bfcd81c6b8e3d2d3e1ae7a3252e1a9715adc9
ask0908 <askdfjaslk@naver.com>
2022-07-25 01:01:17 +0900

    Paging codelab 설정

---
+++ app/src/main/java/com/example/kotlinprac/paging/codelab/advanced/Injection.kt
@@ -0,0 +1,33 @@
+package com.example.kotlinprac.paging.codelab.advanced
+
+import android.content.Context
+import androidx.lifecycle.ViewModelProvider
+import androidx.savedstate.SavedStateRegistryOwner
+import com.example.kotlinprac.paging.codelab.advanced.api.GithubService
+import com.example.kotlinprac.paging.codelab.advanced.data.GithubRepository
+import com.example.kotlinprac.paging.codelab.advanced.db.RepoDatabase
+import com.example.kotlinprac.paging.codelab.advanced.ui.ViewModelFactory
+
+/**
+ * Class that handles object creation.
+ * Like this, objects can be passed as parameters in the constructors and then replaced for
+ * testing, where needed.
+ */
+object Injection {
+
+    /**
+     * Creates an instance of [GithubRepository] based on the [GithubService] and a
+     * [GithubLocalCache]
+     */
+    private fun provideGithubRepository(context: Context): GithubRepository {
+        return GithubRepository(GithubService.create(), RepoDatabase.getInstance(context))
+    }
+
+    /**
+     * Provides the [ViewModelProvider.Factory] that is then used to get a reference to
+     * [ViewModel] objects.
+     */
+    fun provideViewModelFactory(context: Context, owner: SavedStateRegistryOwner): ViewModelProvider.Factory {
+        return ViewModelFactory(owner, provideGithubRepository(context))
+    }
+}
---
+++ app/src/main/java/com/example/kotlinprac/paging/codelab/advanced/db/RepoDao.kt
@@ -0,0 +1,25 @@
+package com.example.kotlinprac.paging.codelab.advanced.db
+
+import androidx.paging.PagingSource
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.kotlinprac.paging.codelab.advanced.model.Repo
+
+@Dao
+interface RepoDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(repos: List<Repo>)
+
+    @Query(
+        "SELECT * FROM repos WHERE " +
+                "name LIKE :queryString OR description LIKE :queryString " +
+                "ORDER BY stars DESC, name ASC"
+    )
+    fun reposByName(queryString: String): PagingSource<Int, Repo>
+
+    @Query("DELETE FROM repos")
+    suspend fun clearRepos()
+}
